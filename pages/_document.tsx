f8197efd5a73f12dc17764553e4950fba465452f
VisheshThapa <visheshjthapa@gmail.com>
2023-01-30 01:02:55 -0800

    Meta Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,12 @@ export default class _Document extends Document {
     return (
       <Html>
         <Head />
-        <title>Otter Inventor: EIT, Engineering Intern and Graduate Jobs</title>
-        <meta name="description" content="Otter Inventor: EIT, Engineering Internships and Graduate Jobs"/>
+        <title>Otter Inventor - EIT, Engineering Intern and Graduate Jobs</title>
+        <meta name="description" content="Otter Inventor - EIT, Engineering Internships and Graduate Jobs"/>
 
         <meta property="og:title" content ="Otter Inventor"/>
         <meta property="og:url" content ="https://www.otterinventor.com/"/>
-        <meta name="og:description" content="Otter Inventor | EIT, Engineering Intern and Graduate Jobs"/>
+        <meta name="og:description" content="Otter Inventor - EIT, Engineering Intern and Graduate Jobs"/>
         <link rel="shortcut icon" href="/logo_icon.ico" />
         <body>
           <Main />
